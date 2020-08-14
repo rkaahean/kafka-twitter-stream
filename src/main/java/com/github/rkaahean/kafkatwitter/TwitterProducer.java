15b82fc4c3e024ab4124476d5db2f5da40cc8ebb
rkaahean <ranjan31051997@gmail.com>
2020-08-14 00:18:30 -0700

    Safe and High Throughput producer

--- src/main/java/com/github/rkaahean/kafkatwitter/TwitterProducer.java
+++ src/main/java/com/github/rkaahean/kafkatwitter/TwitterProducer.java
@@ -25,7 +25,7 @@ import java.util.concurrent.TimeUnit;
 
 public class TwitterProducer {
 
-    List<String> terms = Lists.newArrayList("kafka");
+    List<String> terms = Lists.newArrayList("sancho", "barca", "bayern");
 
     public TwitterProducer(){
 
@@ -144,6 +144,18 @@ public class TwitterProducer {
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // Creating a safe producer
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
+
+        // Creating a high throughput producer
+        // Adding compression and batching
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20"); // 20 ms
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32*1024)); // 32KB batch size
 
         // Create the Producer
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
